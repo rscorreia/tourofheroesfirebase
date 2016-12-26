e0c79e3e1547c0e9c34323d6e2b941ee058683ed
rscorreia <rscorreia@gmail.com>
2016-12-25 23:56:20 -0300

    Angular - Initial Setup modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoDqVJN7nyuxkhR9XCAiqxed9y6Ot2dgg",
+  authDomain: "learning-project-cb831.firebaseapp.com",
+  databaseURL: "https://learning-project-cb831.firebaseio.com",
+  storageBucket: "learning-project-cb831.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +18,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
